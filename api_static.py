92b827fb8efc3e20a9909913ef4a853e1b3da5e0
Mahen Gandhi <mahengandhi19@gmail.com>
2020-04-25 23:20:23 +0530

    added repository object and modified api static

--- api_static.py
+++ api_static.py
@@ -3,6 +3,9 @@ class APIStatic:
     NAME = "name"
     DATA = "data"
     RESOURCE = "resource"
+    ID = "id"
+    LOGIN = "login"
+    TOTAL_COUNT = "totalCount"
 
 
 class RepositoryStatic:
@@ -11,3 +14,11 @@ class RepositoryStatic:
     DISK_USAGE = "diskUsage"
     FORK_COUNT = "forkCount"
     HOMEPAGE_URL = "homepageUrl"
+    IS_ARCHIVED = "isArchived"
+    IS_FORK = "isFork"
+    OWNER = "owner"
+    PRIMARY_LANGUAGE = "primaryLanguage"
+    STARGAZERS = "stargazers"
+    URL = "url"
+    WATCHERS = "watchers"
+    UPDATED_AT = "updatedAt"
--- repository_struct.py
+++ repository_struct.py
@@ -1,12 +1,48 @@
-from pprint import pprint
 from string import Template
 
+from api_static import APIStatic, RepositoryStatic
 from gh_query import GitHubQuery
+from local_settings import AUTH_KEY
 
 
 class Repository:
-    def __init__(self, url, owner_id, name, description, ):
-        pass
+    def __init__(self, created_at, updated_at, disk_usage, url, owner_login, name, description, fork_count,
+                 homepage_url, is_archived, is_fork, primary_language, stargazer_count, watcher_count):
+        self.name = name
+        self.description = description
+        self.fork_count = fork_count
+        self.owner_login = owner_login
+        self.url = url
+        self.watcher_count = watcher_count
+        self.stargazer_count = stargazer_count
+        self.primary_language = primary_language
+        self.is_fork = is_fork
+        self.is_archived = is_archived
+        self.homepage_url = homepage_url
+        self.disk_usage = disk_usage
+        self.updated_at = updated_at
+        self.created_at = created_at
+
+
+def object_decoder(dic) -> Repository:
+    obj = Repository(
+        name=dic[APIStatic.NAME],
+        url=dic[RepositoryStatic.URL],
+        created_at=dic[RepositoryStatic.CREATED_AT],
+        updated_at=dic[RepositoryStatic.UPDATED_AT],
+        description=dic[RepositoryStatic.DESCRIPTION],
+        disk_usage=dic[RepositoryStatic.DISK_USAGE],
+        fork_count=dic[RepositoryStatic.FORK_COUNT],
+        homepage_url=dic[RepositoryStatic.HOMEPAGE_URL],
+        is_archived=dic[RepositoryStatic.IS_ARCHIVED],
+        is_fork=dic[RepositoryStatic.IS_FORK],
+        owner_login=dic[RepositoryStatic.OWNER][APIStatic.LOGIN],
+        primary_language=dic[RepositoryStatic.PRIMARY_LANGUAGE][APIStatic.NAME],
+        stargazer_count=dic[RepositoryStatic.STARGAZERS][APIStatic.TOTAL_COUNT],
+        watcher_count=dic[RepositoryStatic.WATCHERS][APIStatic.TOTAL_COUNT],
+    )
+
+    return obj
 
 
 class RepositoryStruct(GitHubQuery):
@@ -25,6 +61,7 @@ class RepositoryStruct(GitHubQuery):
                        isArchived
                        isFork
                        url
+                       updatedAt
                        owner {
                          login
                          id
@@ -46,11 +83,13 @@ class RepositoryStruct(GitHubQuery):
 
     def __init__(self, github_token, url):
         REPO_QUERY = RepositoryStruct.REPO_QUERY_TEMPLATE.substitute(url=url)
-        super().__init__(github_token,
-                         query=REPO_QUERY)
+        super().__init__(github_token, query=REPO_QUERY)
 
 
 if __name__ == '__main__':
-    repo = RepositoryStruct(github_token="93b717120efc9123208279a6d87b4d50dfe7168d",
+    repo = RepositoryStruct(github_token=AUTH_KEY,
                             url="\"https://github.com/sympy/sympy\"")
-    pprint(dict(next(repo.generator())))
+
+    repo_obj = object_decoder(dict(next(repo.generator())[APIStatic.DATA][APIStatic.RESOURCE]))
+
+    print(repo_obj.name)
