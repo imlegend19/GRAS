eb5fa2469fa163aa0365f8b584f212909c3e6797
Mahen Gandhi <mahengandhi19@gmail.com>
2020-04-26 02:31:47 +0530

    added pagination logic in gh_query.py and fixed bugs in milestone_struct.py

--- gh_query.py
+++ gh_query.py
@@ -1,22 +1,25 @@
 from requests import exceptions, request
+
 from api_static import APIStatic
 
 
 class GitHubQuery:
-
     def __init__(
             self,
             github_token=None,
-            query=None,
+            query_params=None,
+            query=None
     ):
         self.github_token = github_token
         self.query = query
+        self.query_params = query_params
 
     @property
     def headers(self):
         default_headers = dict(
             Authorization=f"token {self.github_token}",
         )
+
         return {
             **default_headers,
         }
@@ -24,12 +27,21 @@ class GitHubQuery:
     def generator(self):
         while True:
             try:
-                yield request(
-                    'post',
-                    APIStatic.BASE_URL,
-                    headers=self.headers,
-                    json=dict(query=self.query)
-                ).json()
+                if self.query_params is None:
+                    yield request(
+                        'post',
+                        APIStatic.BASE_URL,
+                        headers=self.headers,
+                        json=dict(query=self.query)
+                    ).json()
+                else:
+                    print(self.query_params)
+                    yield request(
+                        'post',
+                        APIStatic.BASE_URL,
+                        headers=self.headers,
+                        json=dict(query=self.query.format_map(self.query_params))
+                    ).json()
             except exceptions.HTTPError as http_err:
                 raise http_err
             except Exception as err:
--- milestone_struct.py
+++ milestone_struct.py
@@ -1,58 +1,88 @@
 from pprint import pprint
-from string import Template
 
+from api_static import APIStatic, MilestoneStatic
 from gh_query import GitHubQuery
 from local_settings import AUTH_KEY
 
 
 class Milestone:
     def __init__(self, closed_at, created_at, creator_login, description, due_on, state, title, updated_at, url):
+        super().__init__(closed_at, created_at, creator_login, description, due_on, state, title, updated_at, url)
+
+        self.url = url
+        self.updated_at = updated_at
+        self.title = title
+        self.state = state
+        self.due_on = due_on
+        self.description = description
+        self.creator_login = creator_login
+        self.created_at = created_at
         self.closed_at = closed_at
-# super().__init__(closed_at, created_at, creator_login, description, due_on, state, title, updated_at, url)
 
 
-
-
-def object_decoder(param):
+def object_decoder(dic: dict) -> Milestone:
     pass
 
 
 class MilestoneStruct(GitHubQuery):
-    MILESTONE_QUERY_TEMPLATE = Template(
-        """
-            {
-                repository(name: "$name", owner: "$owner") {
-                    id
-                    createdAt
-                    milestones(orderBy: {field: NUMBER, direction: ASC}, first: 100) {
-                        pageInfo {
-                            hasNextPage
-                            endCursor
-                        }
-                        nodes {
-                            creator {
-                                login
-                                url
-                            }
-                            description
-                            dueOn
+    MILESTONE_QUERY = """
+        {{
+            repository(name: "{name}", owner: "{owner}") {{
+                milestones(first: 100, after: {after}) {{
+                    pageInfo {{
+                        hasNextPage
+                        endCursor
+                    }}
+                    nodes {{
+                        creator {{
+                            login
                             url
-                            title
-                            closedAt
-                            createdAt
-                            number
-                            state
-                            updatedAt
-                        }
-                    }
-                }
-            }
-        """
-    )
+                        }}
+                        description
+                        dueOn
+                        url
+                        title
+                        closedAt
+                        createdAt
+                        state
+                        updatedAt
+                    }}
+                }}
+            }}
+        }}
+    """
 
     def __init__(self, github_token, name, owner):
-        MILESTONE_QUERY = MilestoneStruct.MILESTONE_QUERY_TEMPLATE.substitute(name=name, owner=owner)
-        super().__init__(github_token, query=MILESTONE_QUERY)
+        super().__init__(
+            github_token,
+            query=MilestoneStruct.MILESTONE_QUERY,
+            query_params=dict(name=name, owner=owner, after="null")
+        )
+
+    def iterator(self):
+        generator = self.generator()
+        hasNextPage = True
+        milestones = []
+
+        while hasNextPage:
+            response = next(generator)
+
+            endCursor = response[APIStatic.DATA][APIStatic.REPOSITORY] \
+                [MilestoneStatic.MILESTONES][APIStatic.PAGE_INFO] \
+                [APIStatic.END_CURSOR]
+
+            self.query_params["after"] = "\"" + endCursor + "\""
+
+            milestones.extend(response[APIStatic.DATA]
+                              [APIStatic.REPOSITORY]
+                              [MilestoneStatic.MILESTONES]
+                              [APIStatic.NODES])
+
+            hasNextPage = response[APIStatic.DATA][APIStatic.REPOSITORY] \
+                [MilestoneStatic.MILESTONES][APIStatic.PAGE_INFO] \
+                [APIStatic.HAS_NEXT_PAGE]
+
+        return milestones
 
 
 if __name__ == '__main__':
@@ -60,6 +90,6 @@ if __name__ == '__main__':
                          name="sympy",
                          owner="sympy")
 
-    ms_obj = dict(next(ms.generator()))
+    ms_list = ms.iterator()
 
-    pprint(ms_obj)
+    pprint(len(ms_list))
