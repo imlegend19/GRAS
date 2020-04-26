37cf3a2bf5e7321aa39a918713f4c378d48220bd
Kaustubh Kishore <Kaustubhkishorem@gmail.com>
2020-04-26 20:06:45 +0530

    Repo-Watchers Schema component
    + Added watcher_struct.py:
    + Allows us to retrieve the "Watchers" of a given repository.
    + Following Properties are retrieved:
      -- login
      -- id
    
    + Added "WatcherStatic" inside api_static.py

---
+++ watcher_struct.py
@@ -0,0 +1,65 @@
+from pprint import pprint
+
+from api_static import APIStatic, WatcherStatic
+from gh_query import GitHubQuery
+from local_settings import AUTH_KEY
+
+
+class WatcherStruct(GitHubQuery):
+    WATCHER_QUERY = """
+        {{
+            repository(name: "{name}", owner: "{owner}") {{
+                watchers(first: 100, after: {after}) {{
+                    nodes{{
+                        login
+                        id
+                    }}
+                    pageInfo{{
+                        endCursor
+                        hasNextPage
+                    }}
+                }}
+            }}
+        }}
+    """
+
+    def __init__(self, github_token, name, owner):
+        super().__init__(
+            github_token,
+            query=WatcherStruct.WATCHER_QUERY,
+            query_params=dict(name=name, owner=owner, after="null")
+        )
+
+    def iterator(self):
+        generator = self.generator()
+        hasNextPage = True
+        watchers = []
+
+        while hasNextPage:
+            response = next(generator)
+
+            endCursor = response[APIStatic.DATA][APIStatic.REPOSITORY][
+                WatcherStatic.WATCHERS][APIStatic.PAGE_INFO][APIStatic.END_CURSOR]
+
+            self.query_params["after"] = "\"" + endCursor + "\""
+
+            watchers.extend(response[APIStatic.DATA]
+                            [APIStatic.REPOSITORY]
+                            [WatcherStatic.WATCHERS]
+                            [APIStatic.NODES]
+                            )
+
+            hasNextPage = response[APIStatic.DATA][APIStatic.REPOSITORY][
+                WatcherStatic.WATCHERS][APIStatic.PAGE_INFO][APIStatic.HAS_NEXT_PAGE]
+
+        return watchers
+
+
+if __name__ == '__main__':
+    watcher = WatcherStruct(github_token=AUTH_KEY,
+                            name="sympy",
+                            owner="sympy")
+
+    watcher_list = watcher.iterator()
+
+    pprint(len(watcher_list))
