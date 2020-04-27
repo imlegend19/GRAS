30ce9fe8ddf1e45b1f7ef26e1bdeca192dc7deda
ayankashyap <ayankashyap8@gmail.com>
2020-04-27 13:51:25 +0530

    added milestone,repo,watcher structs

--- cli.py
+++ cli.py
@@ -1,28 +1,51 @@
 from label_struct import LabelStruct
 from language_struct import LanguageStruct
+#from issue_struct import IssueStruct
+from milestone_struct import MilestoneStruct
+from repository_struct import RepositoryStruct
+from watcher_struct import WatcherStruct
+
 from pprint import pprint
 import argparse,sys
 from local_settings import AUTH_KEY
 
 
 parser = argparse.ArgumentParser(description='GITSTRUCT \n gather data from software repositories')
-parser.add_argument("name", type=str,help="name of the repo you wish to parse")
-parser.add_argument("owner",type=str,help="owner of the repo")
+#parser.add_argument("action", type=str,default="generate",help="actions: gen/generate \n")
+parser.add_argument("name",type=str,help="name of the repo ")
+parser.add_argument("owner",type=str,help="owner of the repo ")
 parser.add_argument("-w","--struct", type=str,default="all", help="what you wish to parse")
+parser.add_argument("-l","--limit", type=int,default="100", help="what you wish to parse")
 args = parser.parse_args()
 
-def generate(struct,name,owner):
-    if struct == "all":
-        complete = [LanguageStruct(github_token=AUTH_KEY,name=name,owner=owner).iterator(), 
-        LabelStruct(github_token=AUTH_KEY,name=name,owner=owner).iterator()]
+
+def generate(name,owner):
+    params = (AUTH_KEY,name,owner)
+
+    if args.struct == "all":
+        complete = [LanguageStruct(*params).iterator(), 
+        LabelStruct(*params).iterator(), MilestoneStruct(*params).iterator() ]
         return complete
-    elif struct in ("language", "lang"):
-        lang_list = LanguageStruct(github_token=AUTH_KEY,name=name,owner=owner).iterator()
+    elif args.struct in ("language", "lang"):
+        lang_list = LanguageStruct(*params).iterator()
         return lang_list
-    elif struct in ("label", "lab"):
-        label_list = LabelStruct(github_token=AUTH_KEY,name=name,owner=owner).iterator()
+    elif args.struct in ("label", "lab"):
+        label_list = LabelStruct(*params).iterator()
         return label_list
+    elif args.struct in ("milestone", "miles"):
+        ms_list = MilestoneStruct(*params).iterator()
+        return ms_list
+    elif args.struct in ("repo-data"):
+        repodata_list = RepositoryStruct(AUTH_KEY,"https://github.com/{}/{}".format(owner,name)).iterator()
+        return repodata_list
+    elif args.struct in ("watchers", "watc"):
+        watcher_list = WatcherStruct(*params).iterator()
+        return watcher_list   
+    # elif args.struct in ("issue","iss"):
+    #     issue_list = IssueStruct(*params).iterator(args.limit)
+
+
 
 if __name__ == '__main__':
-    list = generate(args.struct,args.name,args.owner)
+    list = generate(args.name,args.owner)
     sys.stdout.write(str(pprint(list)))
