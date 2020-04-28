3e6220a9227930d4494c08a1f16c522955f5237a
Mahen Gandhi <mahengandhi19@gmail.com>
2020-04-29 01:16:48 +0530

    modified gh_query.py and added private method to send request. It catches all possible errors and also checks whether maximum rate limit has been reached or not.
    Signed-off-by: Mahen Gandhi <mahengandhi19@gmail.com>

--- gh_query.py
+++ gh_query.py
@@ -1,14 +1,15 @@
+import logging
+import time
 from abc import ABCMeta
 from abc import abstractmethod
+from datetime import datetime
 
 from requests import exceptions, request
 
 from api_static import APIStatic
 
 
-class GitHubQuery(object):
-    __metaclass__ = ABCMeta
-
+class GitHubQuery(metaclass=ABCMeta):
     @abstractmethod
     def __init__(
             self,
@@ -21,7 +22,6 @@ class GitHubQuery(object):
         self.query_params = query_params
 
     @property
-    @abstractmethod
     def headers(self):
         default_headers = dict(
             Authorization=f"token {self.github_token}",
@@ -31,24 +31,65 @@ class GitHubQuery(object):
             **default_headers,
         }
 
-    @abstractmethod
+    def __send_request(self, param):
+        tries = 1
+        while tries <= 3:
+            logging.debug("Sending request to base url. (Try: %s)" % tries)
+
+            try:
+                req = request(
+                    'post',
+                    APIStatic.BASE_URL,
+                    headers=self.headers,
+                    json=param
+                )
+            except exceptions.ConnectionError:
+                time.sleep(2)
+                try:
+                    req = request(
+                        'post',
+                        APIStatic.BASE_URL,
+                        headers=self.headers,
+                        json=param
+                    )
+                except exceptions.ConnectionError:
+                    logging.error("Connection Error while fetching data from base url.")
+                    break
+
+            if req.status_code == 200:
+                if 'X-RateLimit-Remaining' in req.headers and int(req.headers['X-RateLimit-Remaining']) <= 2:
+                    reset_time = datetime.fromtimestamp(float(req.headers['X-RateLimit-Reset']))
+                    wait_time = (reset_time - datetime.now()).total_seconds() + 5
+
+                    logging.info("Github API maximum rate limit reached. Waiting for %0.5f sec..." % wait_time)
+                    time.sleep(wait_time)
+
+                    req = request(
+                        'post',
+                        APIStatic.BASE_URL,
+                        headers=self.headers,
+                        json=param
+                    )
+
+                return req.json()
+            else:
+                logging.error("Problem with getting data via base url. Error: %s" % req.text)
+                tries += 1
+                time.sleep(2)
+
+        raise exceptions.RequestException("Problem with getting data via base url.")
+
     def generator(self):
         while True:
             try:
                 if self.query_params is None:
-                    yield request(
-                        'post',
-                        APIStatic.BASE_URL,
-                        headers=self.headers,
-                        json=dict(query=self.query)
-                    ).json()
+                    yield self.__send_request(
+                        param=dict(query=self.query)
+                    )
                 else:
-                    yield request(
-                        'post',
-                        APIStatic.BASE_URL,
-                        headers=self.headers,
-                        json=dict(query=self.query.format_map(self.query_params))
-                    ).json()
+                    yield self.__send_request(
+                        param=dict(query=self.query.format_map(self.query_params))
+                    )
             except exceptions.HTTPError as http_err:
                 raise http_err
             except Exception as err:
