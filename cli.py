351d00fb646a82dab332e9bea7ce14c87b6ffdd4
ayankashyap <ayankashyap8@gmail.com>
2020-04-29 12:01:11 +0530

    cli now accepts repo as owner/name and raises parser error if repo doesnt exist, or isnt in the right format

--- cli.py
+++ cli.py
@@ -4,22 +4,32 @@ from milestone_struct import MilestoneStruct
 from repository_struct import RepositoryStruct
 from watcher_struct import WatcherStruct
 
+
 from pprint import pprint
 import argparse,sys
+import requests
 from local_settings import AUTH_KEY
 
 
 parser = argparse.ArgumentParser(description='GITSTRUCT \n gather data from software repositories')
 # parser.add_argument("action", type=str,default="generate",help="actions: gen/generate \n")
-parser.add_argument("name",type=str,help="name of the repo ")
-parser.add_argument("owner",type=str,help="owner of the repo ")
+parser.add_argument("repo",type=str,help="enter owner and name of the repo in the formate owner/name) ")
+#parser.add_argument("-o","owner",type=str,help="owner of the repo ")
 parser.add_argument("-w","--struct", type=str,default="all", help="what you wish to parse")
 parser.add_argument("-l","--limit", type=int,default="100", help="what you wish to parse")
 args = parser.parse_args()
 
-
-def generate(name,owner):
-    params = (AUTH_KEY,name,owner)
+def repo_check(repo):
+    if ('/' not in repo):
+        parser.error('repo entry must be in the format (owner/name)')
+    elif(not requests.get("https://api.github.com/repos/{}".format(repo)).ok):
+        parser.error("repo does not exist")
+    else:
+        return True
+        
+            
+def generate(repo):
+    params = (AUTH_KEY,*repo.split('/'))
 
     if args.struct == "all":
         complete = [LanguageStruct(*params).iterator(), 
@@ -35,7 +45,7 @@ def generate(name,owner):
         ms_list = MilestoneStruct(*params).iterator()
         return ms_list
     elif args.struct in ("repo-data"):
-        repodata_list = RepositoryStruct(AUTH_KEY,"https://github.com/{}/{}".format(owner,name)).iterator()
+        repodata_list = RepositoryStruct(AUTH_KEY,"https://github.com/{}".format(repo)).iterator()
         return repodata_list
     elif args.struct in ("watchers", "watc"):
         watcher_list = WatcherStruct(*params).iterator()
@@ -47,5 +57,9 @@ def generate(name,owner):
 
 
 if __name__ == '__main__':
-    list = generate(args.name,args.owner)
+    repo_check(args.repo)
+    list = generate(args.repo)
     sys.stdout.write(str(pprint(list)))
+    
+
+
