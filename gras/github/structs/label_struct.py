429fd49a464b6017b54afa9deb9d5e66018f2b8c
Mahen Gandhi <mahengandhi19@gmail.com>
2020-05-17 08:59:06 +0530

    Update label_struct.py

--- gras/github/structs/label_struct.py
+++ gras/github/structs/label_struct.py
@@ -4,25 +4,6 @@ from gras.github.github import GithubInterface
 
 
 class LabelStruct(GithubInterface, LabelModel):
-    """
-        The object models the query to fetch the list of labels associated with a repository and generates an
-        object using
-        :class:`gras.github.entity.github_models.LabelModel` containing the fetched data.
-
-        Please see GitHub's `repository documentation`_, `label connection documentation`_ for more information.
-
-        .. _repository documentation:
-            https://developer.github.com/v4/object/repository/
-
-        .. _label connection documentation:
-            https://developer.github.com/v4/object/labelconnection
-
-        :param name: name of the repository
-        :type name: str
-        :param owner: owner of the repository
-        :type owner: str
-    """
-
     LABEL_QUERY = """
         {{
             repository(name: "{name}", owner: "{owner}") {{
@@ -44,17 +25,34 @@ class LabelStruct(GithubInterface, LabelModel):
     """
 
     def __init__(self, name, owner):
-        """Constructor method
+        """
+        The object models the query to fetch the list of labels associated with a repository and generates an
+        object using :class:`~gras.github.entity.github_models.LabelModel` containing the fetched data.
+
+        Please see GitHub's `repository documentation`_, `label connection documentation`_ for more information.
+
+        .. _repository documentation:
+            https://developer.github.com/v4/object/repository/
+
+        .. _label connection documentation:
+            https://developer.github.com/v4/object/labelconnection
+
+        :param name: name of the repository
+        :type name: str
+
+        :param owner: owner of the repository
+        :type owner: str
         """
         super().__init__(
             query=self.LABEL_QUERY,
             query_params=dict(name=name, owner=owner, after="null"),
-            )
+        )
 
     def iterator(self):
         """
             Iterator function for :class:`gras.github.structs.label_struct.LabelStruct`. For more information see
-            :class:`gras.github.github.githubInterface`.
+            :class:`~gras.github.github.GithubInterface`.
+            
             :return: a single API response or a list of responses
             :rtype: generator<dict>
         """
@@ -80,9 +78,10 @@ class LabelStruct(GithubInterface, LabelModel):
 
     def process(self):
         """
-        generates a :class:`gras.github.entity.github_models.LabelModel` object representing the fetched data.
-        :return: A :class:`gras.github.entity.github_models.LabelModel` object
-        :rtype: class
+        Generates a :class:`~gras.github.entity.github_models.LabelModel` object representing the fetched data.
+        
+        :return: A :class:`~gras.github.entity.github_models.LabelModel` object
+        :rtype: object
         """
 
         for lst in self.iterator():
