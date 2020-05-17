017b7ed64b46ccd6c485879670475a7a30f8f212
Mahen Gandhi <mahengandhi19@gmail.com>
2020-05-17 19:32:56 +0530

    feat(query-engine): Added remove alias function
    Added remove alias funtion to remove a particular alias object give a
    properly indented query and alias name.
    
    Closes #15
    
    Signed-off-by: Mahen Gandhi <mahengandhi19@gmail.com>

--- gras/github/query_builder.py
+++ gras/github/query_builder.py
@@ -3,17 +3,20 @@ from gras.errors import QueryObjectError
 
 class QueryObject:
     def __init__(self, raw=False, object_type=None, object_fields=None, children=None, alias=None,
-                 inline_fragment=False):
+                 inline_fragment=False, key=None, var=None):
         self.object_type = object_type
         self.object_fields = object_fields
         self.children = children
         self.raw = raw
         self.alias = alias
         self.inline_fragment = inline_fragment
-        
+    
+        if key is not None and var is None:
+            raise QueryObjectError(msg="`var` should be specified if `key` is not None.")
+    
         if self.object_type is None and self.raw is None:
             raise QueryObjectError(msg="`raw` or `object_type` have to be specified.")
-        
+    
         self._build()
     
     @staticmethod
@@ -57,13 +60,46 @@ class QueryObject:
     
     def __str__(self):
         return self.query
-    
+
     def aggregate(self, name=None):
         if name:
             final_query = f"{name} " + "{{\n"
         else:
             final_query = "{{\n"
-        
+    
         final_query += self.__formatted_string(self) + "\n}}"
-        
+    
         return final_query
+
+
+def remove_alias(query, alias):
+    alias += ":"
+    lines = query.splitlines()
+    
+    alias_indent = None
+    alias_index = None
+    for i, line in enumerate(lines):
+        if alias in line:
+            alias_indent = len(line) - len(line.lstrip())
+            alias_index = i
+            break
+    
+    if "{{" in lines[alias_index]:
+        lines.pop(alias_indent)
+        
+        index_to_pop = []
+        for i in range(alias_index, len(lines)):
+            indent = len(lines[i]) - len(lines[i].lstrip())
+            if alias_indent < indent:
+                index_to_pop.append(i)
+            elif alias_indent == indent:
+                index_to_pop.append(i)
+                break
+            else:
+                break
+        
+        del lines[index_to_pop[0]: index_to_pop[-1] + 1]
+    else:
+        lines.pop(alias_indent)
+    
+    return "\n".join(lines)
--- tests/test_query_engine.py
+++ tests/test_query_engine.py
@@ -1,6 +1,6 @@
 import unittest
 
-from gras.github.query_builder import QueryObject
+from gras.github.query_builder import QueryObject, remove_alias
 from gras.github.structs.language_struct import LanguageStruct
 
 
@@ -35,6 +35,55 @@ class TestQueryObject(unittest.TestCase):
                 )
             ]
         ).aggregate()
-        
+
         self.assertEqual([x.strip() for x in query.strip().splitlines()],
                          [x.strip() for x in LanguageStruct.LANGUAGE_QUERY.strip().splitlines()])
+
+    def test_alias_removal_from_query(self):
+        result = """{{
+            repository(name: "{name}", owner: "{owner}") {{
+                languages(first: 100, orderBy: {{ field: SIZE, direction: ASC }}, after: {after}) {{
+                    pageInfo {{
+                        endCursor
+                        hasNextPage
+                    }}
+                }}
+            }}
+        }}
+        """
+    
+        query = QueryObject(
+            object_type="repository",
+            object_fields=dict(name="\"{name}\"", owner="\"{owner}\""),
+            children=[
+                QueryObject(
+                    object_type="languages",
+                    object_fields=dict(first=100, orderBy="{{ field: SIZE, direction: ASC }}", after="{after}"),
+                    children=[
+                        QueryObject(
+                            object_type="edges",
+                            alias="languages",
+                            children=[
+                                "size",
+                                QueryObject(
+                                    object_type="node",
+                                    children=["name"]
+                                )
+                            ]
+                        ),
+                        QueryObject(
+                            object_type="pageInfo",
+                            children=[
+                                "endCursor",
+                                "hasNextPage"
+                            ]
+                        )
+                    ]
+                )
+            ]
+        ).aggregate()
+    
+        test = [x.strip() for x in remove_alias(query=query, alias="languages").strip().splitlines()]
+        result = [x.strip() for x in result.strip().splitlines()]
+    
+        self.assertEqual(test, result)
