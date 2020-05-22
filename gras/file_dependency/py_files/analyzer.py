568b14866827d2c1101d6305262a36130581a961
ayankashyap <ayankashyap8@gmail.com>
2020-05-23 01:42:11 +0530

    small refactor

--- gras/file_dependency/py_files/analyzer.py
+++ gras/file_dependency/py_files/analyzer.py
@@ -1,8 +1,6 @@
 from gras.file_dependency.utils import lines_of_code_counter
 from gras.file_dependency.py_files.node_parser import parse_dir
+from pprint import pprint
 
 # TODO: run this, maybe put parse_dir in this file, I am not sure how to structure this anymore
-#parse_dir('/home/viper/dev/GRAS/gras/file_dependency')
-
-
-
+pprint(parse_dir('/home/viper/dev/GRAS/gras/file_dependency'))
